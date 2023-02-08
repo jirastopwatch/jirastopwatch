172335015d68fe605c6e5e53dc7d91bd3233fe24
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2023-02-09 12:18:07 +1300

    Delete modal.js
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- docs/js/modal.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $('#myModal').on('show.bs.modal', function (e) {
-    var image = $(e.relatedTarget).attr('src');
-    $(".img-responsive").attr("src", image);
-  });
-})
