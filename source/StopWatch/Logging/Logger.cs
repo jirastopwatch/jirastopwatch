9b1977215192ee08c8676a91b10877dd508a2535
Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>
2023-02-23 16:24:13 +1300

    Update copyright notice
    Signed-off-by: Y. Meyer-Norwood <106889957+norwd@users.noreply.github.com>

--- source/StopWatch/Logging/Logger.cs
+++ source/StopWatch/Logging/Logger.cs
@@ -1,18 +1,25 @@
-﻿/**************************************************************************
-Copyright 2016 Carsten Gehling
+/**
+ * Copyright 2023 Y. Meyer-Norwood
+ * Copyright 2020 Dan Tulloh
+ * Copyright 2016 Carsten Gehling
+ *
+ * For a full list of contributing authors, see:
+ *
+ *     https://jirastopwatch.github.io/contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-**************************************************************************/
 using System;
 using System.IO;
 using System.Threading;
